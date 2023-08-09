c401d8c296cab1923310e316791721d8258db751
Thauane <thauane-souza1@live.com>
2023-08-09 09:33:23 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+import "./styles/estilosGlobais.scss";
 import PaginaInicial from "./paginas/paginasInicial";
 
 export default function App() {
-  return <PaginaInicial/>;
+  return <PaginaInicial />;
 }
--- src/paginas/paginasInicial/index.jsx
+++ src/paginas/paginasInicial/index.jsx
@@ -1,7 +1,24 @@
 import Cabecalho from "../../components/cabecalho";
+import Menu from "../../components/Menu";
+import banner from "./banner.png";
+import styles from "./PaginaInicial.module.scss";
+import Rodape from "../../components/Rodape";
 
 export default function PaginaInicial(){
     return(
+        <>
         <Cabecalho/>
+        <main>
+            <section className={styles.principal}>
+            <Menu/>
+            <div className={styles.principal__imagem}>
+                <h1>A galeria mais completa do espaço</h1>
+                <img src={banner} alt="A imagem da terra vista do espaço" />
+            </div>
+            </section>
+
+        </main>
+        <Rodape />
+        </>
     )
 }
